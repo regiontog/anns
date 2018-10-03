572bad5cf7419e149e10beed7bd6e09b8a5ab2c8
Jontahan <jona.jorg@gmail.com>
2018-10-04 01:06:03 +0200

    glass, yeast and poker

---
+++ yeast/dataset.py
@@ -0,0 +1,22 @@
+from it3105.abc import Dataset
+from os import path
+import csv
+
+def bitvec(n):
+    return [1 if n == m else 0 for m in range(10)]
+
+class WineDataset(Dataset):
+    def __init__(self, path):
+        with open(path) as f:
+            data = [row for row in csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)]
+            self.samples = [row[:-1] for row in data]
+            self.labels = [bitvec(row[-1] - 1) for row in data]
+    
+    def nth_case(self, n):
+        return self.samples[n], self.labels[n]
+
+    @property
+    def size(self):
+        return len(self.samples)
+
+wine = WineDataset(path.join(path.dirname(__file__), 'yeast/yeast.txt'))
