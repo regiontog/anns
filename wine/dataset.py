9b7c17c6b033aeedfa9d4731aba98676fe5e9072
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2018-10-04 04:14:38 +0200

    Update specs to new file format(visualization parameters)

--- wine/dataset.py
+++ wine/dataset.py
@@ -2,6 +2,7 @@ from it3105.abc import Dataset
 from os import path
 import csv
 
+
 def bitvec(n):
     return [1 if n == m else 0 for m in range(6)]
 
@@ -9,11 +10,12 @@ def bitvec(n):
 class WineDataset(Dataset):
     def __init__(self, path):
         with open(path) as f:
-            data = [row for row in csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)]
-            print(data[3])
-            self.samples = [row[:-1] for row in data] 
+            data = [row for row in csv.reader(
+                f, delimiter=';', quoting=csv.QUOTE_NONNUMERIC)]
+
+            self.samples = [row[:-1] for row in data]
             self.labels = [bitvec(row[-1]) for row in data]
-    
+
     def nth_case(self, n):
         return self.samples[n], self.labels[n]
 
@@ -21,4 +23,6 @@ class WineDataset(Dataset):
     def size(self):
         return len(self.samples)
 
-wine = WineDataset(path.join(path.dirname(__file__), 'wine/winequality_red.txt'))
+
+wine = WineDataset(path.join(path.dirname(
+    __file__), 'wine/winequality_red.txt'))
